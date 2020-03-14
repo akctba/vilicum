c9355d559cfc02572aec150c169f64932d34d669
Alex Kayser <akctba@gmail.com>
2020-03-14 01:04:59 -0700

    ignore class

--- src/main/java/br/org/vilicum/view/FamiliaEditMB.java
+++ src/main/java/br/org/vilicum/view/FamiliaEditMB.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
 import br.gov.frameworkdemoiselle.annotation.PreviousView;
 import br.gov.frameworkdemoiselle.message.MessageContext;
 import br.gov.frameworkdemoiselle.message.SeverityType;
@@ -12,11 +15,6 @@ import br.gov.frameworkdemoiselle.template.AbstractEditPageBean;
 import br.gov.frameworkdemoiselle.transaction.Transactional;
 import br.org.vilicum.business.FamiliaBC;
 import br.org.vilicum.business.MembroBC;
-import br.org.vilicum.correios.AtendeCliente;
-import br.org.vilicum.correios.AtendeClienteService;
-import br.org.vilicum.correios.EnderecoERP;
-import br.org.vilicum.correios.SQLException_Exception;
-import br.org.vilicum.correios.SigepClienteException;
 import br.org.vilicum.domain.Estados;
 import br.org.vilicum.domain.Familia;
 import br.org.vilicum.domain.Membro;
@@ -29,71 +27,79 @@ public class FamiliaEditMB extends AbstractEditPageBean<Familia, Long> {
 
 	@Inject
 	private FamiliaBC familiaBC;
-	
+
 	@Inject
 	private MembroBC membroBC;
-	
+
 	@Inject
 	private MessageContext messageContext;
-	
+
 	@Override
 	@Transactional
 	public String delete() {
 		this.familiaBC.delete(getId());
 		return getPreviousView();
 	}
-	
+
 	@Override
 	@Transactional
 	public String insert() {
 		this.familiaBC.insert(getBean());
 		return ""; // vazio para manter na mesma pagina getPreviousView();
 	}
-	
+
 	@Override
 	@Transactional
 	public String update() {
 		this.familiaBC.update(getBean());
-		return ""; //getPreviousView();
+		return ""; // getPreviousView();
 	}
-	
+
 	@Override
 	protected Familia handleLoad(Long id) {
 		return this.familiaBC.load(id);
 	}
-	
+
 	public void buscaCep() {
-		
+
 		Familia fam = this.getBean();
-		
+
 		String cep = fam.getCep();
-		
-		if (cep!=null && cep.length() == 9) { //considerando o hifen
+
+		if (cep != null && cep.length() == 9) { // considerando o hifen
+
 			try {
-				AtendeClienteService service = new AtendeClienteService();
-				AtendeCliente atendeCliente = service.getAtendeClientePort();
-				EnderecoERP endereco = atendeCliente.consultaCEP(cep);
+				Unirest.setTimeouts(0, 0);
+				HttpResponse<String> response = Unirest
+						.post("https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl")
+						.header("Content-Type", "application/xml")
+						.body("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" \n\txmlns:cli=\"http://cliente.bean.master.sigep.bsb.correios.com.br/\">\n <soapenv:Header/>\n <soapenv:Body>\n <cli:consultaCEP>\n <cep>81710310</cep>\n </cli:consultaCEP>\n </soapenv:Body>\n</soapenv:Envelope> ")
+						.asString();
+
 				
-				if (endereco != null) {
-					fam.setRua(endereco.getEnd());
-					fam.setBairro(endereco.getBairro());
-					fam.setCidade(endereco.getCidade());
-									
-					Estados estado = Estados.parseEstado(endereco.getUf());
-					
-					fam.setEstado(estado);
-					
-				} else {
-					//FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "CEP não encontrado: " + cep, null));
-					messageContext.add("CEP não encontrado: " + cep, SeverityType.INFO);
-					fam.setRua("CEP não encontrado");
-				}
-			} catch (SQLException_Exception e) {
-				messageContext.add("Erro ao buscar CEP: " + cep, SeverityType.INFO);
-				System.out.println("SQLException_Exception " + e.getMessage());
-				e.printStackTrace();
-				fam.setRua("Erro ao buscar CEP");
-			} catch (SigepClienteException e) {
+
+
+//				
+//				if (endereco != null) {
+//					fam.setRua(endereco.getEnd());
+//					fam.setBairro(endereco.getBairro());
+//					fam.setCidade(endereco.getCidade());
+//									
+//					Estados estado = Estados.parseEstado(endereco.getUf());
+//					
+//					fam.setEstado(estado);
+//					
+//				} else {
+//					//FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "CEP não encontrado: " + cep, null));
+//					messageContext.add("CEP não encontrado: " + cep, SeverityType.INFO);
+//					fam.setRua("CEP não encontrado");
+//				}
+//			} catch (SQLException_Exception e) {
+//				messageContext.add("Erro ao buscar CEP: " + cep, SeverityType.INFO);
+//				System.out.println("SQLException_Exception " + e.getMessage());
+////				e.printStackTrace();
+//				fam.setRua("Erro ao buscar CEP");
+			} catch (Exception e) {
 				messageContext.add("Erro ao buscar CEP: " + cep, SeverityType.INFO);
 				System.out.println("SigepClienteException " + e.getMessage());
 				e.printStackTrace();
@@ -101,11 +107,11 @@ public class FamiliaEditMB extends AbstractEditPageBean<Familia, Long> {
 			}
 		}
 	}
-	
+
 	public Estados[] getEstados() {
 		return Estados.values();
 	}
-	
+
 	public List<Membro> ativos() {
 		return membroBC.ativos(getBean());
 	}
